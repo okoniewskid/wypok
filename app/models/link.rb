ad12430a7e93996ad57e1d68fbcc4da7353e0661
romety2 <romety2@wp.pl>
2016-10-28 21:40:55 +0000

    #10 usuniety warning

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,8 +4,8 @@ class Link < ApplicationRecord
     has_many :comments, :dependent => :delete_all
     
     def self.search(search)
-    		where("title LIKE ?", "%#{search}%") 
-  	end
+        where("title LIKE ?", "%#{search}%") 
+    end
    
     validates :title, :url, presence: true
     validates :url, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/, message: "jest niewłaściwy" }
