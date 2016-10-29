e7fd84a1a468fdac8ea0d11d0c934003435ec0a6
romety2 <romety2@wp.pl>
2016-10-29 10:56:27 +0000

    #13 dodano procentowe wartosci strony

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,15 @@ class Link < ApplicationRecord
     def self.search(search)
         where("title LIKE ?", "%#{search}%") 
     end
+    
+    def ci_lower_bound(pos, n, confidence)
+        if n == 0
+            return 0.0
+        end
+        z = Statistics2.pnormaldist(1-(1-confidence)/2)
+        phat = 1.0*pos/n
+        ((phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)*100).round(2)
+    end
    
     validates :title, :url, presence: true
     validates :url, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/, message: "jest niewłaściwy" }
