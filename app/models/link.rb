b0b541ada58838a56cbb9bb36a1e747b7ae46dd3
romety2 <romety2@wp.pl>
2016-10-29 13:59:49 +0000

    #11 porawiono strukture (dodano procedure generujaca obrazek)

--- app/models/link.rb
+++ app/models/link.rb
@@ -15,6 +15,20 @@ class Link < ApplicationRecord
         phat = 1.0*pos/n
         ((phat + z*z/(2*n) - z * Math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)*100).round(2)
     end
+    
+    def generate_image(link)
+        begin
+          @object = LinkThumbnailer.generate(link.url) 
+          @e = false
+          rescue
+            @e = true
+        end
+        if !@e && @object.images && @object.images.first
+            @object.images.first.src.to_s
+        else
+            "http://s10.ifotos.pl/img/brakzdjec_sapeppe.gif"
+        end
+    end
    
     validates :title, :url, presence: true
     validates :url, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/, message: "jest niewłaściwy" }
