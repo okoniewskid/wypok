bba8df6af63ed1e097ce1f6d5e196f0bc71a3f3c
romety2 <romety2@wp.pl>
2016-11-05 23:40:15 +0000

    #18 dziala poprawnie bius w kazdej sytuacji

--- vendor/assets/javascripts/bius.js
+++ vendor/assets/javascripts/bius.js
@@ -1,43 +1,31 @@
-var myBIUS = function()
+document.getElementById("boldButtom").addEventListener("click", function (event2)
 {
-    document.getElementById("boldButtom").addEventListener("click", function (event2)
-    {
-        var text = document.getElementById("myText");
-        text.value = text.value.substring(0, text.selectionStart) +
-        '[b]'+text.value.substring(text.selectionStart, text.selectionEnd) +
-        '[/b]'+text.value.substring(text.selectionEnd);
-    });
-        
-    document.getElementById("italicButtom").addEventListener("click", function (event2)
-    {
-        var text = document.getElementById("myText");
-        text.value = text.value.substring(0, text.selectionStart) +
-        '[i]'+text.value.substring(text.selectionStart, text.selectionEnd) +
-        '[/i]'+text.value.substring(text.selectionEnd);
-    });
-        
-    document.getElementById("underlineButtom").addEventListener("click", function (event2)
-    {
-        var text = document.getElementById("myText");
-        text.value = text.value.substring(0, text.selectionStart) +
-        '[u]'+text.value.substring(text.selectionStart, text.selectionEnd) +
-        '[/u]'+text.value.substring(text.selectionEnd);
-    });
-      
-    document.getElementById("strikethroughButtom").addEventListener("click", function (event2)
-    {
-        var text = document.getElementById("myText");
-        text.value = text.value.substring(0, text.selectionStart) +
-        '[s]'+text.value.substring(text.selectionStart, text.selectionEnd) +
-        '[/s]'+text.value.substring(text.selectionEnd);
-    });
-}
-
-window.addEventListener("load", function (event)
+    var text = document.getElementById("myText");
+    text.value = text.value.substring(0, text.selectionStart) +
+    '[b]'+text.value.substring(text.selectionStart, text.selectionEnd) +
+    '[/b]'+text.value.substring(text.selectionEnd);
+});
+            
+document.getElementById("italicButtom").addEventListener("click", function (event2)
 {
-    myBIUS();
-    document.addEventListener("turbolinks:load", function (event)
-    {
-        myBIUS();
-    });
+    var text = document.getElementById("myText");
+    text.value = text.value.substring(0, text.selectionStart) +
+    '[i]'+text.value.substring(text.selectionStart, text.selectionEnd) +
+    '[/i]'+text.value.substring(text.selectionEnd);
+});
+            
+document.getElementById("underlineButtom").addEventListener("click", function (event2)
+{
+    var text = document.getElementById("myText");
+    text.value = text.value.substring(0, text.selectionStart) +
+    '[u]'+text.value.substring(text.selectionStart, text.selectionEnd) +
+    '[/u]'+text.value.substring(text.selectionEnd);
+});
+          
+document.getElementById("strikethroughButtom").addEventListener("click", function (event2)
+{
+    var text = document.getElementById("myText");
+    text.value = text.value.substring(0, text.selectionStart) +
+    '[s]'+text.value.substring(text.selectionStart, text.selectionEnd) +
+    '[/s]'+text.value.substring(text.selectionEnd);
 });
