c7239565dd408d296f1d2272e4bb29ad837467a4
romety2 <romety2@wp.pl>
2016-11-06 01:00:38 +0000

    naprawiony blad juz dziala dodawanie i edycja linkow

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,12 @@
 class Link < ApplicationRecord
     acts_as_votable
-    include SimpleHashtag::Hashtaggable
     belongs_to :user
     has_many :comments, :dependent => :delete_all
    
     validates :title, :url, presence: true
     validates :url, format: { with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/, message: "jest niewłaściwy" }
     
+        
     def self.search(search)
         where("title LIKE ?", "%#{search}%") 
     end
