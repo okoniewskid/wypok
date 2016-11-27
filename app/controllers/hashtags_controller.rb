9a4bd6c5a785e4dde566b31a869fe8dc0e029c29
romety2 <romety2@wp.pl>
2016-11-27 23:45:40 +0000

    #29 (poprzedni tez #29) dziala sortowanie hasztagow wg daty

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -3,12 +3,45 @@ require 'will_paginate/array'
 class HashtagsController < ApplicationController
 
   def index
-    @hashtags = SimpleHashtag::Hashtag.all.sort { |x,y| x.name <=> y.name }
+    case
+      when params[:sortName]
+        case params[:sortName]
+          when "asc"
+            @hashtags = SimpleHashtag::Hashtag.all.order('name ASC')
+            @sn = false;
+          when "desc"
+            @hashtags = SimpleHashtag::Hashtag.all.order('name DESC')
+            @sn = true;
+        end
+      when params[:sortCount]
+        case params[:sortCount]
+          when "asc"
+            @hashtags = SimpleHashtag::Hashtag.all.group('name').order('COUNT(id) ASC')
+            @sc = false;
+          when "desc"
+            @hashtags = SimpleHashtag::Hashtag.all.group('name').order('COUNT(id) DESC')
+            @sc = true;
+        end
+      else
+        @hashtags = SimpleHashtag::Hashtag.all.order('name ASC')
+    end
   end
 
   def show
     @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
-    @hashtagged = @hashtag.hashtaggables.paginate(page: params[:page], per_page: 10) if @hashtag
+    case
+      when params[:sortDate]
+        case params[:sortDate]
+          when "desc"
+            @hashtagged = @hashtag.hashtaggables.paginate(page: params[:page], per_page: 10) if @hashtag
+            @sd = false;
+          when "asc"
+            @hashtagged = @hashtag.hashtaggables.reverse.paginate(page: params[:page], per_page: 10) if @hashtag
+            @sd = true;
+        end
+      else
+        @hashtagged = @hashtag.hashtaggables.paginate(page: params[:page], per_page: 10) if @hashtag
+    end
     respond_to do |format|
       format.html
       format.js
