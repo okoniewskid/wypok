52c1bb48f6b76406349e336ce761086d3bbde2b6
romety2 <romety2@wp.pl>
2016-12-18 10:08:13 +0000

    #39 poprawienie struktury edycji konta

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,13 @@ class User < ApplicationRecord
       self.add_role(:admin) if self.roles.blank?
     end
   end
+  
+  def update_with_password(params={}) 
+    if params[:password].blank? 
+      params.delete(:password) 
+      params.delete(:password_confirmation) if params[:password_confirmation].blank? 
+    end 
+    update_attributes(params) 
+  end
+
 end
