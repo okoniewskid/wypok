1c93fe2aeb28f22068e58558512bdda2f6ef476c
romety2 <romety2@wp.pl>
2017-01-09 10:47:48 +0000

    dodano napis administrator w panelu uzytkownika

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     def show
       @user = User.find(params[:id])
+      if @user.has_role? :admin
+        @adminRole = true
+      else
+        @adminRole = false
+      end
       @links = Link.all.where("user_id = "+params[:id])
     end
 end
