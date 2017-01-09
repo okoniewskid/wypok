1cca6c6a93418df8d76bc95fe5a303d45336fe59
mtyczynski <mikit14@gmail.com>
2017-01-09 15:53:00 +0000

    avatar fix przy rejestracji
    Signed-off-by: mtyczynski <mikit14@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,10 @@ ActiveRecord::Schema.define(version: 20161217081907) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.string   "avatar"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
