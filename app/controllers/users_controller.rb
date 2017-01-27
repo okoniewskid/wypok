17fd159fa27c3a959564c836cd06dc378b51b5b3
romety2 <romety2@wp.pl>
2017-01-27 22:28:40 +0000

    #41 poprawiono wyglad oraz naprawiono bledy w panelu admina

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -175,6 +175,33 @@ include HashtagsHelper
     
     def update
       @user = User.find(params[:id])
+      if @user.has_role? :admin
+        @adminRole = true
+      else
+        @adminRole = false
+      end
+      if @user.has_role? :block
+        @blockRole = true
+      else
+        @blockRole = false
+      end
+      if @user.has_role? :email
+        @emailRole = true
+      else
+        @emailRole = false
+      end
+      if current_user
+        u = User.find(current_user.id)
+        if u.has_role? :admin
+          @allow = true
+        else
+          @allow = false
+          flash[:notice] = "Brak uprawnień!"
+        end
+      else
+        @allow = false
+        flash[:notice] = "Brak uprawnień!"
+      end
       if @user.update_attributes(params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation))
         if params[:admin]
           if !(@user.has_role? :admin)
